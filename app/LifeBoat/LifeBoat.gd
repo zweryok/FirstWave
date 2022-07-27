f0c5e2d99cd3dc40a3556e49cb682c78d4a57d23
zweryok <z@debian.debian.local>
2022-07-27 22:57:39 +0400

    update

--- app/LifeBoat/LifeBoat.gd
+++ app/LifeBoat/LifeBoat.gd
@@ -1,7 +1,7 @@
-extends RigidBody
+extends RigidDynamicBody3D
 
 var FUEL = 1600 # Масса топлива
-var fuel_burning = 0 #Сжигание толива 
+var fuel_burning = 0
 
 var THRUST = 8000 # Тяга
 var trust_input = 0
@@ -22,7 +22,7 @@ func get_input(_delta):
 	$LifeBoat_JetStream.hide(); $LifeBoat_JetStream2.hide();
 	$LifeBoat_JetStream3.hide(); $LifeBoat_JetStream4.hide()
 	
-	if Input.is_action_pressed("ui_trust"):
+	if Input.is_action_pressed("ui_trust") and FUEL>0:
 		trust_input = THRUST
 		$LifeBoat_JetStream.show(); $LifeBoat_JetStream2.show()
 		$LifeBoat_JetStream3.show(); $LifeBoat_JetStream4.show()
@@ -34,22 +34,23 @@ func get_input(_delta):
 	if Input.is_action_pressed("ui_down"):
 		pitch_input +=100; $LifeBoat_JetStream.show()
 	if Input.is_action_pressed("yaw_right"):
-		yaw_input -=1; $LifeBoat_JetStream3.show()
+		yaw_input -=100; $LifeBoat_JetStream3.show()
 	if Input.is_action_pressed("yaw_left"):
-		yaw_input +=1; $LifeBoat_JetStream4.show()
+		yaw_input +=100; $LifeBoat_JetStream4.show()
 	if Input.is_action_pressed("ui_right"):
-		roll_input +=1
+		roll_input +=100
 	if Input.is_action_pressed("ui_left"):
-		roll_input -=1
+		roll_input -=100
 	
 func _integrate_forces(state):
 	get_input(state.step)
-	add_central_force(transform.basis.z * trust_input)
-	add_torque(transform.basis.x * pitch_input)
-	add_torque(transform.basis.y * yaw_input)
-	add_torque(-transform.basis.z * roll_input)
+	apply_central_force(transform.basis.z * trust_input)
+	apply_torque(transform.basis.x * pitch_input)
+	apply_torque(transform.basis.y * yaw_input)
+	apply_torque(-transform.basis.z * roll_input)
 	#Сжигание топлива
-	#fuel_burning = (abs(trust_input)+abs(pitch_input)*5 +abs(yaw_input)*5 +abs(roll_input)*5)*0.002*get_process_delta_time()
-	#set_mass(get_mass() - fuel_burning)
-	#FUEL = FUEL - fuel_burning
-	print("speed", ceil (linear_velocity.length()*3),"fuel",ceil(FUEL))
+	fuel_burning = (abs(trust_input)+abs(pitch_input)*5 +abs(yaw_input)*5 +abs(roll_input)*5)*0.002*get_process_delta_time()
+	set_mass(get_mass() - fuel_burning)
+	FUEL = FUEL - fuel_burning
+	print("speed", ceil (linear_velocity.length()*40))
+#,"fuel",ceil(FUEL)
